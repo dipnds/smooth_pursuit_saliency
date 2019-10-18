393b22e2ef1f4d4734a5d67d8bd263b724801bc0
John Ridley <john.ridley@tum.de>
2019-10-18 13:57:27 +0200

    Refactored data loader

--- vid2npy.py
+++ vid2npy.py
@@ -1,93 +1,99 @@
-import skvideo.io
 from scipy.io import arff
 from scipy import ndimage
-import os
-import numpy as np
-import cv2
 from sklearn.model_selection import train_test_split
-import pickle
-# import matplotlib.pyplot as plt
 
-# H = 528; L = 720 # frame dim
-H = 72; L = 128 # reduced frame dimension
-frame_mean = np.zeros([1,H,L,3]); frame_std = np.zeros([1,H,L,3])
-frame_count = 0;
-seq = 100; shift = 50
-file_count = 0
+import os, cv2, tqdm, skvideo.io, pathlib
+import numpy as np
 
-for vid_list in os.listdir('all_videos'):
-    vidnpy = skvideo.io.vread('all_videos/'+vid_list)
-    
-##    resize, see Mikhail
-    vid = np.zeros([vidnpy.shape[0],H,L,3],dtype=np.int32)
-    for num,frame in enumerate(vidnpy):
-        vid[num,:,:,:] = cv2.resize(frame,(L,H))
-    
-##    pad, see Mikhail not doing this
-#    vid = np.zeros([vidnpy.shape[0],H,L,3],dtype=np.int32)
-#    err = [int((H-vidnpy.shape[1])/2),int((L-vidnpy.shape[2])/2)]
-#    vid[:,err[0]:err[0]+vidnpy.shape[1],err[1]:err[1]+vidnpy.shape[2],:] = vidnpy
-    
-    frame_count += vid.shape[0]
-    frame_mean += np.sum(vid, axis=0)
-    frame_std += np.sum(vid**2, axis=0)
-    
-#    plt.imshow(vid[0,:,:,:]); plt.show()
-    
-    op = np.zeros(vid.shape[0:-1])
-    gt = np.zeros(vidnpy.shape[0:-1])
-    for anno in os.listdir('output_sp_tool_50_files/test/'+vid_list[0:-4]+'/'): # annotator loop
-        lab = arff.loadarff('output_sp_tool_50_files/test/'+vid_list[0:-4]+'/'+anno)
-        interv = lab[0].size/frame_count
-        for i in range(vid.shape[0]): # frame loop
-            
-            ## correcting gaze for resizing
-            y = lab[0]['y'][(int(np.round(i*interv))):int(np.round((i+1)*interv))]
-            x = lab[0]['x'][(int(np.round(i*interv))):int(np.round((i+1)*interv))]
-            for j in range(x.shape[0]):
-                if 0<=y[j]<vidnpy.shape[1]-1 and 0<=x[j]<vidnpy.shape[2]-1:
-                    gt[i,int(np.round(y[j])),int(np.round(x[j]))] += 1
-                
-    sigma_t = 24.75/3; sigma_s = 26.178 # see Mikhail for presets
-    temp = gt.copy()
-    ndimage.gaussian_filter(gt,sigma=[sigma_t, sigma_s, sigma_s], output=gt) # spatio-temporal smoothing
-    for num,frame in enumerate(gt):
-        op[num,:,:] = cv2.resize(gt[num,:,:],(L,H))
-#    skvideo.io.vwrite('check.mp4',(255*op/np.max(op)).astype(np.uint8))
-    op = 2*op/np.max(op) - 1 # normalised per video
+class DataLoader:
+    def __init__(self, name, files):
+        self.name = name
+        pathlib.Path(f'{name}/ip/').mkdir(parents=True, exist_ok=True)
+        pathlib.Path(f'{name}/op/').mkdir(parents=True, exist_ok=True)
+        self.files = files
+        self.frame_count = 0 
+        self.frame_std = 0
+        self.frame_height = 72
+        self.frame_width = 128
 
-##            per frame Gaussian blur, dropped            
-##            correcting gaze for padding
-#            y = np.minimum((lab[0]['y'][(int(np.round(i*interv))):int(np.round((i+1)*interv))]) + err[0], H-1)
-#            x = np.minimum((lab[0]['x'][(int(np.round(i*interv))):int(np.round((i+1)*interv))]) + err[1], L-1)
-#            for j in range(x.shape[0]):
-#                op[i,int(np.round(y[j])),int(np.round(x[j]))] += 1           
-#            op[i,:,:] = cv2.GaussianBlur(op[i,:,:],(13,13),9) # change third 3 (sigma) as per exisitng code
-                
-    print(op.shape)
-    
-    count = int(np.ceil((vid.shape[0] - seq)/shift)) + 1
-    for i in range(count):
-        file_count += 1
-        np.save('ip/vid'+(str(100000+file_count))[1:]+'_'+vid_list[0:-4]+'.npy',
-                vid[i*shift:np.min((i*shift+seq,vid.shape[0])),:,:,:])
-        np.save('op/lab'+(str(100000+file_count))[1:]+'_'+vid_list[0:-4]+'.npy',
-                op[i*shift:np.min((i*shift+seq,vid.shape[0])),:,:])
+        self.frame_mean = np.zeros([1, self.frame_height, self.frame_width, 3])
+        self.frame_std = np.zeros_like(self.frame_mean)
 
-frame_mean /= frame_count
-frame_std = np.sqrt((frame_std - frame_mean**2)/frame_count)
-np.save('frame_mean.npy',frame_mean)
-np.save('frame_std.npy',frame_std)
+        self.seq_len = 100
+        self.shift = 50
+        self.file_count = 0
 
-for vid_list in os.listdir('ip'):
-    ip = np.load('ip/'+vid_list)
-    ip = (ip - frame_mean) / frame_std
-    np.save('ip/'+vid_list,ip)
+    def load(self):
+        print(f'Loading {self.name}')
+        self.file_progress = tqdm.tqdm(self.files)
+        for f in self.file_progress:
+            raw_video = skvideo.io.vread(f'all_videos/{f}')
+            self.file_progress.set_description(f'Resizing: {f}')
+            video = self.resize(raw_video, [raw_video.shape[0], self.frame_height, self.frame_width, 3], np.int32)
 
-temp = os.listdir('ip'); temp.sort()
-split = train_test_split(temp,test_size=0.1,shuffle=False) # eval has 10% of train data
-list_tr = split[0]; list_ev = split[1]
-with open('list_tr.pkl', 'wb') as f:
-    pickle.dump(list_tr, f)
-with open('list_ev.pkl', 'wb') as f:
-    pickle.dump(list_ev, f)
+            self.frame_count += raw_video.shape[0]
+            self.frame_mean += np.sum(video, axis=0)
+            self.frame_std += np.sum(video ** 2, axis=0)
+
+            self.file_progress.set_description(f'Calculating Saliency: {f}')
+            saliencyMap = self.makeSaliency(f, raw_video.shape)
+            self.file_progress.set_description(f'Writing: {f}')
+            for i in range(int(np.ceil((video.shape[0] - self.seq_len)/self.shift)) + 1):
+                self.file_count += 1
+                np.save(f'{self.name}/ip/{self.file_count:03d}_vid{f[0:-4]}.npy', video[i*self.shift:np.min((i*self.shift+self.seq_len,video.shape[0])),:,:,:])
+                np.save(f'{self.name}/op/{self.file_count:03d}_lab{f[0:-4]}.npy', saliencyMap[i*self.shift:np.min((i*self.shift+self.seq_len,video.shape[0])),:,:])
+
+
+        self.frame_mean /= self.frame_count
+        self.frame_std = np.sqrt((self.frame_std - self.frame_mean**2)/self.frame_count)
+        self.frame_std[self.frame_std==0] = 1
+
+    def resize(self, video, shape, dtype):
+        resized = np.zeros(shape, dtype=dtype)
+        if len(shape) == 4:
+            for num, frame in enumerate(video):
+                resized[num, :, :, :] = cv2.resize(frame, (shape[2], shape[1]))
+        else:
+            for num, frame in enumerate(video):
+                resized[num, :, :] = cv2.resize(frame, (shape[2], shape[1]))
+        return resized
+
+    def makeSaliency(self, f, shape):
+        saliencyMap = np.zeros(shape[0:-1])
+        data_files = os.listdir(f'output_sp_tool_50_files/test/{f[0:-4]}/')
+        for data_file in data_files:
+            labels = arff.loadarff(f'output_sp_tool_50_files/test/{f[0:-4]}/{data_file}')
+            interval = labels[0].size / shape[0]
+            for i in range(shape[0]): # frame loop
+                y = labels[0]['y'][(int(np.round(i*interval))):int(np.round((i+1)*interval))]
+                x = labels[0]['x'][(int(np.round(i*interval))):int(np.round((i+1)*interval))]
+                saliencyMap[i, :, :], _, _ = np.histogram2d(y, x, bins=(shape[1], shape[2]), range=[[-0.5, shape[1] - 0.5], [-0.5, shape[2] - 0.5]])
+
+        sigma_t = 24.75/3; sigma_s = 26.178 # see Mikhail for presets
+        ndimage.gaussian_filter(saliencyMap, sigma=[sigma_t, sigma_s, sigma_s], output=saliencyMap) # spatio-temporal smoothing
+        resized = self.resize(saliencyMap, [saliencyMap.shape[0], self.frame_height, self.frame_width], np.float64)
+        skvideo.io.vwrite('check.mp4',(255*resized/np.max(resized)).astype(np.uint8))
+        resized = 2*resized/np.max(resized) - 1 # normalised per video
+        return resized
+
+    def normaliseFrames(self, loader):
+        for vid_file in os.listdir(f'{self.name}/ip'):
+            ip = np.load(f'{self.name}/ip/{vid_file}')
+            ip = (ip - loader.frame_mean) / loader.frame_std
+            np.save(f'{self.name}/ip/{vid_file}', ip)
+
+    def writeDistribution(self):
+        np.save('frame_mean.npy', self.frame_mean)
+        np.save('frame_std.npy', self.frame_std)
+
+
+train_videos, eval_videos = train_test_split(os.listdir('all_videos'), test_size=0.1, shuffle=False) # eval has 10% of train data
+
+train_set = DataLoader('train', train_videos)
+train_set.load()
+train_set.normaliseFrames(train_set)
+train_set.writeDistribution()
+
+eval_set = DataLoader('eval', eval_videos)
+eval_set.load()
+eval_set.normaliseFrames(train_set)
