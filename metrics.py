31d6ef894a800fe546b3ed8c14fea7eb0360f317
Deepan Das <deepan.das@tum.de>
2019-11-06 18:39:53 +0100

    NSS code

---
+++ metrics.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def NSS(op, pred):
+    ma = pred - pred.mean(dim=(1, 2, 3, 4),keepdim=True)
+    sd = pred.std(dim=(1, 2, 3, 4),keepdim=True); sd[sd==0] = 1
+    ma = ma/sd
+    op = (op+1)/2
+    score = (ma * (op.sign())).mean().cpu().detach().numpy()
+    return score
